0b69ee3f7a202f7cb19161ceb0befb0e19abb028
favour musenga <favourmusenga@gmail.com>
2020-08-07 00:53:03 +0200

    added pug

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
 
+app.set("view engine", "pug");
+
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
@@ -12,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
+  res.render("index");
 });
 
 app.listen(process.env.PORT || 3000, () => {
